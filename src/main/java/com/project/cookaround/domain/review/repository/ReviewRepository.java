90bf2c21ee7bb0a5f98bf0401772a52d3b1a8eb3
yy-ham <yyham.dev@gmail.com>
2025-11-07 12:08:51 +0900

    feat: 마이페이지 내가 쓴 글/후기 요약 박스에 내 평균별점 표시 형식 변경
    - 소수점 한 자리까지만 표시하는 것으로 변경
    
    #14

--- src/main/java/com/project/cookaround/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/project/cookaround/domain/review/repository/ReviewRepository.java
@@ -21,7 +21,7 @@ public class ReviewRepository {
 
     // 사용자가 작성한 레시피에 달린 후기들의 평균
     public Double findAverageRatingByMemberId(Long memberId) {
-        return em.createQuery("select avg(rv.rating) from Review rv where rv.recipe.id in (" +
+        return em.createQuery("select round(avg(rv.rating), 1) from Review rv where rv.recipe.id in (" +
                         "select id from Recipe rc where rc.member.id = :memberId)", Double.class)
                 .setParameter("memberId", memberId)
                 .getSingleResult();
